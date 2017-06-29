b3710e5b1e886f2fca8b2e9f9e7c0e16051fcba6
Poobaloofa <julian.ivaldi@gmail.com>
2017-06-29 10:53:41 -0400

    Update plat.js

--- plat.js
+++ plat.js
@@ -2,7 +2,7 @@ var c = document.getElementById('canvas');
 var ctx = c.getContext('2d');
 var movespeed = 3
 var globalGravity = .2
-var scroll = 0
+var xscroll = 0
 var player = {
   x: c.width / 2,
   y: c.height / 2,
@@ -58,7 +58,7 @@ var player = {
       this.xspeed = -movespeed
       for (var i = 0; i < platforms.length; i++) {
         if (this.y < platforms[i].botSide && this.y + this.size > platforms[i].topSide) {
-          if (this.x - movespeed < platforms[i].rSide - scroll && this.x + movespeed > platforms[i].rSide - scroll) {
+          if (this.x - movespeed < platforms[i].rSide - xscroll && this.x + movespeed > platforms[i].rSide - xscroll) {
             this.xspeed = 0
           }
         }
@@ -69,7 +69,7 @@ var player = {
       this.xspeed = movespeed
       for (var i = 0; i < platforms.length; i++) {
         if (this.y < platforms[i].botSide && this.y + this.size > platforms[i].topSide) {
-          if (this.x + this.size < platforms[i].lSide - scroll + movespeed && this.x + this.size > platforms[i].lSide - scroll - movespeed) {
+          if (this.x + this.size < platforms[i].lSide - xscroll + movespeed && this.x + this.size > platforms[i].lSide - xscroll - movespeed) {
             this.xspeed = 0
           }
         }
@@ -85,7 +85,7 @@ var player = {
 
     //collision/platforms
     for (var i = 0; i < platforms.length; i++) {
-      if /* x is within x of plat */ (this.x < platforms[i].rSide-scroll && this.x + this.size > platforms[i].lSide-scroll) {
+      if /* x is within x of plat */ (this.x < platforms[i].rSide-xscroll && this.x + this.size > platforms[i].lSide-xscroll) {
         if /* y is inside plat */ (this.y + this.yspeed < platforms[i].botSide && this.y + this.size + this.yspeed > platforms[i].topSide) {
           console.log("what the actual frick")
           if (this.yspeed > 0) {
@@ -100,17 +100,17 @@ var player = {
       }
       if /* y is inside plat y */ (this.y < platforms[i].botSide && this.y + this.size > platforms[i].topSide) {
         console.log("heck this dude")
-        if /* x will be within x of plat */ (this.x + this.xspeed < platforms[i].rSide-scroll && this.x + this.size + this.xspeed > platforms[i].lSide-scroll) {
+        if /* x will be within x of plat */ (this.x + this.xspeed < platforms[i].rSide-xscroll && this.x + this.size + this.xspeed > platforms[i].lSide-xscroll) {
           console.log("y in y and x in x")
           if (this.xspeed > 0) {
-            this.x = platforms[i].lSide-scroll - this.size
+            this.x = platforms[i].lSide-xscroll - this.size
             this.xspeed = 0
           }
           if (this.xspeed < 0) {
-            this.x = platforms[i].rSide-scroll
+            this.x = platforms[i].rSide-xscroll
             this.xspeed = 0
           }
-          //problem: pressing left/right keys clips into block and triggers vertical collision
+          //problem (RESOLVED): pressing left/right keys clips into block and triggers vertical collision
         }
       }
     }
@@ -129,59 +129,100 @@ function platform(startx, starty, width, height) {
   this.width = width
   this.height = height
   this.update = function() {
-    ctx.fillRect(this.lSide-scroll, this.topSide, this.width, this.height)
+    ctx.fillRect(this.lSide-xscroll, this.topSide, this.width, this.height)
   }
 }
 
 function addPlat(startx, starty, width, height) {
   platforms.push(new platform(startx, starty, width, height))
 }
-addPlat(0, 450, 450, 50)
-addPlat(300, 250, 200, 50)
-addPlat(300, 250, 50, 150)
-addPlat(100, 350, 200, 50)
-addPlat(50, 150, 150, 50)
-addPlat(0, -1,  2000, 1)
-addPlat(500,200,50,100)
-addPlat(550,150,150,150)
-addPlat(600,100,50,100)
-addPlat(600,100,100,50)
-addPlat(700,0,500,1000)
-addPlat(-1000,0,1000,1000)
-addPlat(-100,900,900,100)
-addPlat(650,450,50,50)
-addPlat(650,0,50,100)
 
-//drawing death sign
-var deathSign = new Image
-deathSign.src = "https://i.imgur.com/pogZi87.jpg"
-deathSign.x = 375
-deathSign.y = 375
+//all mapping
+var img = new Image();
+img.src ='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAAAUCAIAAACVui2AAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QYdDioOvrLUQAAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAx0lEQVRYw+WYSw6AIAxEO4b7X7kuNNEARSiUj7AVcF5oOwWizQaIiJmzpgKZM02EArqFb80Ajt1OeBlgXXCFq5x14A3MguhwM5xDfaLmi0HB1Jea+auXhIDo57EFuZ5ZEn8DM3NIODmzhJ3W/ABHCT+ZFSnnuWLPAuED65gXa7O8xuOK7ZlNpb0tRfP5Nzn82NJeVVpi7lNIxvhwT/U14VNU2MO/oGfrq0u5hi547eCsL71plVKrZ/sAYGTxba8frR4Adrv/0wnPXZAWm8hNPQAAAABJRU5ErkJggg==';
+ctx.drawImage(img, 0, 0);
+var imgData = ctx.getImageData(0, 0, img.width, img.height);
+var scale = 1;
 
-//drawing smile
-var smile = new Image
-smile.src = "https://cdn.discordapp.com/attachments/233670630879395841/329782430762401795/smilepart2.png"
-smile.x = 800
-smile.y = 400
+function pixel(){
+this.r=undefined
+this.g=undefined
+this.b=undefined
+this.a=undefined
+}
 
+var map =[];
+for(var i = 0; i<img.height; i++){
+map.push([])
+}
+for(var y = 0; y<img.height; y++){
+for(var x = 0; x<img.width; x++){
+map[y].push(new pixel)
+}
+}
+
+for (var y = 0; y < img.height; y++) {
+  for (var x = 0; x < img.width; x++) {
+    map[y][x].r = imgData.data[(y * img.width + x) * 4 + 0]
+    map[y][x].g = imgData.data[(y * img.width + x) * 4 + 1]
+    map[y][x].b = imgData.data[(y * img.width + x) * 4 + 2]
+    map[y][x].a = imgData.data[(y * img.width + x) * 4 + 3];
+  }
+}
+console.log(map)
+
+for(var y = 0; y<map.length; y++){
+	for(var x = 0; x<map[y].length; x++){
+     if(map[y][x].r == 0 && map[y][x].g == 0 && map[y][x].b == 0 && map[y][x].a == 255){
+   	addPlat((x/map.length)*c.height*scale,(y/map.length)*c.height*scale,c.height*scale/map.length,c.height*scale/map.length);
+   }
+  }
+}
+
+//sprites
+function sprite(x,y,src){
+this.x=x
+this.y=y
+this.img = new Image
+this.img.src = src
+}
+
+var backgroundSprites = []
+var foregroundSprites = []
+
+function addBackground(x,y,src){
+backgroundSprites.push(new sprite(x,y,src))
+}
+function addForeground(x,y,src){
+foregroundSprites.push(new sprite(x,y,src))
+}
+/*
+//deathsign
+addBackground(375,375,"https://i.imgur.com/pogZi87.jpg")
+//smiley
+addForeground(830,400,"https://cdn.discordapp.com/attachments/233670630879395841/329782430762401795/smilepart2.png")
+*/
 setInterval(function() {
   ctx.clearRect(0, 0, c.width, c.height);
   if (player.y<c.height){
   if (player.x>300){
-  scroll+= movespeed
+  xscroll+= movespeed
   player.x -= movespeed
   }
-  if (player.x<50){
-  scroll-= movespeed
+  if (player.x<100){
+  xscroll-= movespeed
   player.x += movespeed
   }}
-  ctx.drawImage(deathSign, 375-scroll, 375)
+  for(var i = 0; i<backgroundSprites.length;i++){
+  ctx.drawImage(backgroundSprites[i].img,backgroundSprites[i].x-xscroll,backgroundSprites[i].y)
+  }
   player.update();
   for (var i = 0; i < platforms.length; i++) {
-    if (platforms[i].rSide-scroll > 0 && platforms[i].lSide-scroll < c.width)
+    if (platforms[i].rSide-xscroll > 0 && platforms[i].lSide-xscroll < c.width)
       platforms[i].update()
-  ctx.drawImage(smile, 830-scroll, 400)
   }
+  for(var i = 0; i<foregroundSprites.length;i++){
+  ctx.drawImage(foregroundSprites[i].img,foregroundSprites[i].x-xscroll,foregroundSprites[i].y)}
 }, 10);
 
 var jumpable = true

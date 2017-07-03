9f2135d678f56485e8bb930f6b1c7497dcfac97e
Poobaloofa <julian.ivaldi@gmail.com>
2017-07-03 15:31:08 -0400

    new player icon, lighting sorta weird

--- draw.js
+++ draw.js
@@ -4,7 +4,7 @@ function draw(){
     ctx.clearRect(platforms[i].lSide - camera.x, platforms[i].topSide - camera.y, platforms[i].width, platforms[i].height);
     ctx.drawImage(platforms[i].sprite,platforms[i].lSide - camera.x, platforms[i].topSide - camera.y)
   }
-  ctx.clearRect(player.x - camera.x, player.y - camera.y, player.size, player.size)
+  ctx.drawImage(playersprite, player.x - camera.x, player.y - camera.y)
   ctx.drawImage(sassage,23530 - camera.x,330-camera.y)
   light(player.x-camera.x,player.y-camera.y,200);
 }

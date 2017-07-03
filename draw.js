26ce8785f79e097f1e457372512ea010b71ae5b9
All88keys <chrisvass1@gmail.com>
2017-07-03 12:35:06 -0400

    fixed rside

--- draw.js
+++ draw.js
@@ -1,7 +1,8 @@
 function draw(){
   ctx.drawImage(background, 0, 0)
   for (var i = 0; i < platforms.length; i++) {
-    ctx.drawImage(platforms[i].sprite,platforms[i].lSide - camera.x, platforms[i].rSide - camera.y)
+    ctx.clearRect(platforms[i].lSide - camera.x, platforms[i].topSide - camera.y, platforms[i].width, platforms[i].height);
+    ctx.drawImage(platforms[i].sprite,platforms[i].lSide - camera.x, platforms[i].topSide - camera.y)
   }
   ctx.clearRect(player.x - camera.x, player.y - camera.y, player.size, player.size)
 }

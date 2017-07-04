0386b4bb29e1aba7d863eadec53e2a3f3104ca1b
All88keys <chrisvass1@gmail.com>
2017-07-04 17:24:04 -0400

    added fps

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var stats = new Stats();
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+stats.dom.style.position = 'absolute';
+stats.dom.style.float = 'right';
+document.getElementById('game').appendChild( stats.dom );
+
+
 var fps = 100;
 var globalGravity = 0.2 // 0.2 pixels/second/second
 var movespeed = 3 // 3 pixels/second
@@ -7,11 +14,15 @@ window.onload= function(){
   c.width= 1280; //rendering resolution
   c.height= 720;
   setInterval(function(){
+    stats.begin();
+
     checkInputs();
     pUpdate(player);
     camera.update();
     draw();
     document.getElementById('x').innerHTML = "X: " + Math.floor(player.x);
     document.getElementById('y').innerHTML = "Y: " + Math.floor(-player.y);
+
+  	stats.end();
   }, 1000/fps);
 }

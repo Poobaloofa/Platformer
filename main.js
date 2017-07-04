68249d0922445da2c855572c1ac13d6f34f564f5
All88keys <chrisvass1@gmail.com>
2017-07-03 21:43:26 -0400

    cleaned up UI

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+var fps = 100;
 var globalGravity = 0.2 // 0.2 pixels/second/second
 var movespeed = 3 // 3 pixels/second
-var player = new Player
+var player = new Player;
 window.onload= function(){
   mapToArray();
-  c.width=500;
-  c.height=500;
+  c.width= 1280; //rendering resolution
+  c.height= 720;
   setInterval(function(){
     checkInputs();
     pUpdate(player);
     camera.update();
     draw();
-    console.log(player.x + " , " + player.y);
-  }, 10);
+    document.getElementById('x').innerHTML = "X: " + Math.floor(player.x);
+    document.getElementById('y').innerHTML = "Y: " + Math.floor(-player.y);
+  }, 1000/fps);
 }
